1a37a118462c4f63d0f1ba2a4033dd9adbaff1ed
Martin Garton <martin.garton@ft.com>
2016-08-18 11:21:29 +0100

    Use github.com/stretchr/testify/assert

--- extract_test.go
+++ extract_test.go
@@ -2,9 +2,13 @@ package logfilter
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRepoMirrorLogExample(t *testing.T) {
+	assert := assert.New(t)
+
 	in := `172.31.30.229 - - [19/Jun/2015:09:24:24 +0000] "GET /v1/images/f467d023d63178a6686daab33049b7fec024f88e5b64898e9c89dafaaa4e1d8a/ancestry HTTP/1.1" 200 1836 "-" "docker/1.5.0 go/go1.3.3 git-commit/a8a31ef-dirty kernel/3.19.3 os/linux arch/amd64"`
 
 	out, ok := extractAccEntry(in)
@@ -13,35 +17,35 @@ func TestRepoMirrorLogExample(t *testing.T) {
 		t.Fatal("failed to extract values")
 	}
 
-	if out.Status != 200 {
-		t.Errorf("expected status %d but got %d\n", 200, out.Status)
-	}
+	assert.Equal(200, out.Status)
 	// TODO:
 }
 
 func TestMethodeAPIExample(t *testing.T) {
+	assert := assert.New(t)
+
 	in := `127.0.0.1 - - [21/Apr/2015:12:15:34 +0000] "GET /eom-file/all/e09b49d6-e1fa-11e4-bb7f-00144feab7de HTTP/1.1" 200 53706 919 919`
 
 	out, ok := extractAccEntry(in)
 	if !ok {
 		t.Fatal("failed to extract values")
 	}
-	if out.Status != 200 {
-		t.Errorf("expected status %d but got %d\n", 200, out.Status)
-	}
+
+	assert.Equal(200, out.Status)
 	// TODO:
 }
 
 func TestCmsNotifierPostExample(t *testing.T) {
+	assert := assert.New(t)
+
 	in := `172.17.42.1 -  -  [24/Jun/2015:11:09:36 +0000] "POST /notify HTTP/1.1" 500 - "-" "curl/7.42.0" 2197`
 	out, ok := extractAccEntry(in)
-	t.Logf("out status value %v", out.Status)
+
 	if !ok {
 		t.Fatal("failed to extract values")
 	}
-	if out.Status != 500 {
-		t.Errorf("expected status %d but got %d\n", 500, out.Status)
-	}
+
+	assert.Equal(500, out.Status)
 	// TODO:
 }
 
