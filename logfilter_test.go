07a1185f603b343cb3dd825e028d2c6fe536b929
Sorin Buliarca <sorin.buliarca@ft.com>
2018-08-07 13:51:53 +0300

    Renamed test method.

--- logfilter_test.go
+++ logfilter_test.go
@@ -170,7 +170,7 @@ func TestContainsBlacklistedStringWithoutBlacklistedString(t *testing.T) {
 
 func TestBlacklistedServices(t *testing.T) {
 	for blacklistedService := range blacklistedServices {
-		msg := msgWithService(blacklistedService)
+		msg := msgWithContainerName(blacklistedService)
 		m := make(map[string]interface{})
 		json.Unmarshal([]byte(msg), &m)
 		assert.False(t, processMessage(m))
@@ -183,7 +183,7 @@ func TestNotBlacklistedServices(t *testing.T) {
 		processed  bool
 	}{
 		{
-			jsonString: msgWithService("publish-availability-monitor"),
+			jsonString: msgWithContainerName("publish-availability-monitor"),
 			processed:  true,
 		},
 	}
@@ -326,7 +326,7 @@ func TestBypassWithoutAPIKeysInURLQueryParams(t *testing.T) {
 	assert.Equal(t, expectedMsg, actualMsg)
 }
 
-func msgWithService(service string) string {
+func msgWithContainerName(service string) string {
 	return fmt.Sprintf(`{"CONTAINER_ID":"03d1f4078733","CONTAINER_ID_FULL":"03d1f4078733f75f4505b07d1f8a3e8287ed497d9d54e0e785440cb969378ca3","CONTAINER_NAME":"k8s_%s_%s-79d574774-2rxrj_kube-system_a093cbca-fb5a-11e7-a6b6-06263dd4a414_6","CONTAINER_TAG":"gcr.io/google_containers/cluster-autoscaler@sha256:6ceb111a36020dc2124c0d7e3746088c20c7e3806a1075dd9e5fe1c42f744fff","HOSTNAME":"ip-10-172-40-164.eu-west-1.compute.internal","MACHINE_ID":"8d1225f40ee64cc7bcce2f549a41657c","MESSAGE":"I0119 15:38:05.932385 1 leaderelection.go:199] successfully renewed lease kube-system/cluster-autoscaler","POD_NAME":"cluster-autoscaler-79d574774-2rxrj","SYSTEMD_UNIT":"docker.service","_SOURCE_REALTIME_TIMESTAMP":"1516376285932645","_SYSTEMD_INVOCATION_ID":"e3b2703c430f45e8a7075dbcf6b3a588","environment":"upp-prod-publish-eu","platform":"up-coco"}`, service, service)
 }
 
